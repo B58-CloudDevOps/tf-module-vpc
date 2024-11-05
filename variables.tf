9ffde33747e360f78551e8aec7c6a3aacf9f7e42
CodingManoj <CodingManoj@gmail.com>
2024-11-04 20:55:50 -0500

    Deploys ArgoCD

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
-variable "env" {}
 variable "vpc_cidr_block" {}
-
 variable "web_subnet_cidr" {}
 variable "app_subnet_cidr" {}
 variable "db_subnet_cidr" {}
+
+variable "env" {}
+variable "tags" {}
