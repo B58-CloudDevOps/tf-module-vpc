54549d2bf23d55b8400ffd376a8f2fc315adecea
CodingManoj <CodingManoj@gmail.com>
2024-11-04 21:01:51 -0500

    Deploys ArgoCD

---
+++ locals.tf
@@ -0,0 +1,5 @@
+locals {
+  tags = merge(var.tags, { module_name = "tf-module-vpc" })
+
+  name_tag_prefix = var.env
+}
--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${local.name_tag_prefix}-vpc"
+    }
+  )
 }
