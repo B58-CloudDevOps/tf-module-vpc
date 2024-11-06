decccd3968b1f1faf3994257c9dacfb311797ae3
CodingManoj <CodingManoj@gmail.com>
2024-11-05 20:23:32 -0500

    Deploys ArgoCD

--- route.tf
+++ route.tf
@@ -3,3 +3,31 @@ resource "aws_route" "default-vpc" {
   destination_cidr_block    = var.vpc_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
+
+resource "aws_route_table" "web" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block                = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+}
+
+resource "aws_route_table" "app" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block                = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+}
+
+
+resource "aws_route_table" "db" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block                = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+}
