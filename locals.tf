505bc34e87f3cc6d64efb6574403900a09e235c2
CodingManoj <CodingManoj@gmail.com>
2024-11-05 19:46:25 -0500

    Deploys ArgoCD

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   tags     = merge(var.tags, { module_name = "tf-module-vpc" })
-  vpc_tags = merge(local.tags, { module_name = "${var.env}-vpc" })
+  vpc_tags = merge(local.tags, { Name = "${var.env}-vpc" })
 
   name_tag_prefix = var.env
 }
