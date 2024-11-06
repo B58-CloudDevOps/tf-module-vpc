a303f7bc92282d0119cf30f25fe59d11fa5bfae5
CodingManoj <CodingManoj@gmail.com>
2024-11-05 20:27:33 -0500

    Deploys ArgoCD

--- route.tf
+++ route.tf
@@ -4,6 +4,7 @@ resource "aws_route" "default-vpc" {
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
 
+# Creates web route table
 resource "aws_route_table" "web" {
   vpc_id = aws_vpc.main.id
 
@@ -11,6 +12,15 @@ resource "aws_route_table" "web" {
     cidr_block                = var.default_vpc_cidr
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   }
+}
+
+# Creates web rt association to web subnets
+
+resource "aws_route_table_association" "web" {
+  count = length(aws_subnet.web.*.id)
+
+  subnet_id      = aws_subnet.web.*.id[count.index]
+  route_table_id = aws_route_table.web.id
 }
 
 resource "aws_route_table" "app" {
@@ -22,6 +32,14 @@ resource "aws_route_table" "app" {
   }
 }
 
+# Creates app rt association to app subnets
+
+resource "aws_route_table_association" "app" {
+  count = length(aws_subnet.app.*.id)
+
+  subnet_id      = aws_subnet.app.*.id[count.index]
+  route_table_id = aws_route_table.app.id
+}
 
 resource "aws_route_table" "db" {
   vpc_id = aws_vpc.main.id
@@ -30,4 +48,13 @@ resource "aws_route_table" "db" {
     cidr_block                = var.default_vpc_cidr
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   }
+}
+
+# Creates db rt association to db subnets
+
+resource "aws_route_table_association" "db" {
+  count = length(aws_subnet.db.*.id)
+
+  subnet_id      = aws_subnet.db.*.id[count.index]
+  route_table_id = aws_route_table.db.id
 }
